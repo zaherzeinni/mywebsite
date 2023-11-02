fa6075db4b4f6323d90046ac3d95b50da6bf8cb8
Zaher Zeinni <zaherzeinni@hotmail.com>
2023-11-02 20:03:21 +0200

    fdfsdf

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      domains: [
+        "localhost",
+        "res.cloudinary.com",
+      
+        "firebasestorage.googleapis.com",
+      ],
+    },
+    // typescript: {
+    //   // !! WARN !!
+    //   // Dangerously allow production builds to successfully complete even if
+    //   // your project has type errors.
+    //   // !! WARN !!
+    //   ignoreBuildErrors: true,
+    // },
+  };
+  
+  module.exports = nextConfig;
