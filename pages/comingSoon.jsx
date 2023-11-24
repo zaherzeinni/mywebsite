13ed82a9b7b321eb14093f6e3f01ed30e43676ff
Zaher Zeinni <zaherzeinni@hotmail.com>
2023-11-24 23:30:15 +0200

    coming soon

--- pages/comingSoon.jsx
+++ pages/comingSoon.jsx
@@ -1,103 +1,152 @@
-import { Container } from "postcss";
-import React, { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
+import { FaChampagneGlasses } from "react-icons/fa6";
 
+const Countdown = ({}) => {
+  const [primaryColor, setPrimaryColor] = useState("#061826");
+  const [secondaryColor, setSecondaryColor] = useState("#2C4053");
+  const [textColor, setTextColor] = useState("#ffffff");
+  const [startTime, setStartTime] = useState(Date.now() + 48 * 3600 * 1000);
+  console.log("setPrimaryColor", startTime);
+  const [timeRemaining, setTimeRemaining] = useState(
+    // dummy time remaining
+    {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    }
+  );
 
-const CountDown = () => {
-  const [countDays, setCountDays] = useState("00");
-  const [countHours, setCountHours] = useState("00");
-  const [countMinutes, setCountMinutes] = useState("00");
-  const [countSeconds, setCountSeconds] = useState("00");
-
-
-  let interval = useRef();
-  const startTimer = ({ yearTrget, monthTrget, dayTrget }) => {
-    const countdowndate = new Date(
-      `${yearTrget},${monthTrget},${dayTrget},  00:00:00`
-    ).getTime();
-    interval = setInterval(() => {
-      const now = new Date().getTime();
-      const distance = countdowndate - now;
-
-
-      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      const hours = Math.floor(
-        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      if (distance < 0) {
-        clearInterval(interval.current);
-      } else {
-        //update Timer
-        setCountDays(days);
-        setCountHours(hours);
-        setCountMinutes(minutes);
-        setCountSeconds(seconds);
-      }
-    }, 1000);
+  const calculateTimeRemaining = () => {
+    const timeRemaining = startTime - Date.now();
+    // console.log(timeRemaining);
+    return {
+      days: Math.floor(timeRemaining / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((timeRemaining / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((timeRemaining / 1000 / 60) % 60),
+      seconds: Math.floor((timeRemaining / 1000) % 60),
+    };
   };
 
-
+  // Update time remaining every second
   useEffect(() => {
-    let intervalClean = interval.current;
-    startTimer({ yearTrget: "2023", monthTrget: "11", dayTrget: "31" });
+    const interval = setInterval(() => {
+      setTimeRemaining(calculateTimeRemaining());
+      console.log("Time remaining" + timeRemaining);
+    }, 1000);
+
+    //    return interval
     return () => {
-      clearInterval(intervalClean);
+      console.log("CLEAR TIME");
+      clearInterval(interval);
     };
-  });
+  }, [startTime]);
+
   return (
-  
+    <div className=" min-h-screen flex   justify-center items-center bg-black">
+      {/* container */}
+      <div
+        className="flex w-[80%] z-30 md:w-6/12 bg-primary rounded-md count sm:px-2 px-1 md:mt-32"
+        style={{ backgroundColor: primaryColor }}
+      >
+        {/* Countdown Titles */}
+        <div className="contain-tile w-1/4 h-full flex flex-col justify-center items-center pt-3 pb-1">
+          <div
+            className="bg-secondary aspect-square w-11/12 rounded-md flex justify-center items-center"
+            style={{ backgroundColor: secondaryColor }}
+          >
+            <span
+              className="transition  text-7xl font-bold text-white flex justify-center items-center"
+              style={{ color: textColor }}
+              suppressHydrationWarning={true}
+            >
+              {timeRemaining.days}
+            </span>
+          </div>
+          <span
+            className="transition  text-2xl font-semibold text-white flex justify-center items-center"
+            style={{ color: textColor }}
+          >
+            Days
+          </span>
+        </div>
 
-<div className=" w-full h-auto min-h-screen object-cover object-center ">
-      {/* ---content--- */}
+        <div className="contain-tile w-1/4 h-full flex flex-col justify-center items-center pt-3 pb-1">
+          <div
+            className="bg-secondary aspect-square w-11/12 rounded-md flex justify-center items-center"
+            style={{ backgroundColor: secondaryColor }}
+          >
+            <span
+              className="transition  text-7xl font-bold text-white flex justify-center items-center"
+              style={{ color: textColor }}
+              suppressHydrationWarning={true}
+            >
+              {timeRemaining.hours}
+            </span>
+          </div>
+          <span
+            className="transition count-other text-2xl font-semibold text-white flex justify-center items-center"
+            style={{ color: textColor }}
+          >
+            Hours
+          </span>
+        </div>
 
+        <div className="contain-tile w-1/4 h-full flex flex-col justify-center items-center pt-3 pb-1">
+          <div
+            className="bg-secondary aspect-square w-11/12 rounded-md flex justify-center items-center"
+            style={{ backgroundColor: secondaryColor }}
+          >
+            <span
+              className="transition count-text text-7xl font-bold text-white flex justify-center items-center"
+              style={{ color: textColor }}
+              suppressHydrationWarning={true}
+            >
+              {timeRemaining.minutes}
+            </span>
+          </div>
+          <span
+            className="transition count-other text-2xl font-semibold text-white flex justify-center items-center"
+            style={{ color: textColor }}
+          >
+            Minutes
+          </span>
+        </div>
 
-      <img
-        className=" w-full !h-full min-h-screen object-cover object-center"
-        src="https://images.unsplash.com/photo-1468436139062-f60a71c5c892?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt=""
-      />
-
-
-      <div className="text-center justify-center items-center flex h-[100vh]  absolute md:top-[3px]  md:left-[1060px] top-[200px] left-[185px]">
-        <div className="bg-red-400">
-<div>    Days
-</div>
-
-<div>
-    Hours
-</div>
-
-<div>
-    Minutes
-</div>
-
-<div>
-    Seconds
-</div>
-
-</div>
+        <div className="contain-tile w-1/4 h-full flex flex-col justify-center items-center pt-3 pb-1">
+          <div
+            className="bg-secondary aspect-square w-11/12 rounded-md flex justify-center items-center"
+            style={{ backgroundColor: secondaryColor }}
+          >
+            <span
+              className="transition count-text text-7xl font-bold text-white flex justify-center items-center"
+              style={{ color: textColor }}
+              suppressHydrationWarning={true}
+            >
+              {timeRemaining.seconds}
+            </span>
+          </div>
+          <span
+            className="transition count-other text-2xl font-semibold text-white flex justify-center items-center"
+            style={{ color: textColor }}
+          >
+            Seconds
+          </span>
+        </div>
       </div>
 
-
-
+      <img
+        className="absolute object-cover w-full h-full top-0 left-0 -my-32"
+        alt="banner sekcji"
+        src={
+          "https://images.unsplash.com/photo-1531297484001-80022131f5a1?q=80&w=1640&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        }
+      />
+      <div className="relative text-white left-7 top-14 font-bold text-xl">
+        Coming Soon...   ITPROMAX.com
+      </div>
     </div>
-
-
-    
   );
 };
 
-
-export default CountDown;
-
-
-
-
-
-
-//primary #061826"
-
-//Secondary #2C4053
-
-//text color #ffffff
+export default Countdown;
