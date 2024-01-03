14a95875c7e81e5c5f75db3311b0821e8315c45b
Zaher Zeinni <zaherzeinni@hotmail.com>
2024-01-03 21:08:20 +0200

    `update`

--- components/client/sections/support.jsx
+++ components/client/sections/support.jsx
@@ -1,72 +1,54 @@
-import React from "react";
-import { SimpleGrid, Box, Card, Flex, Center } from "@chakra-ui/react";
-import Image from "next/image";
+import React from 'react';
+import { IoChatboxEllipsesSharp } from "react-icons/io5";
+import { FaPhone } from "react-icons/fa6";
+import { FaCalendar } from "react-icons/fa";
+import { Card, SimpleGrid ,Button,Box} from '@chakra-ui/react';
+
 const Support = () => {
-  return (
-    <div>
-      <SimpleGrid spacing={10} columns={[1, 1, 2, 3]} mb={10}>
-
-        <Card p={22} className=" !shadow-lg">
-          <Flex gap={10} mx={15}>
-            <Box >
-              <Image
-                src="/support/truck.png"
-                width={64}
-                height={64}
-                className="!w-[64px] !h-[64px]"
-              />
+    return (
+        <div className='my-12 md:w-[60%] m-auto w-[80%] '>
+            <SimpleGrid columns={[1,1,3,3]} gap={5}>
+            
+            <Card flexDirection={'column'} borderRadius={10} gap={5} className='my-6 md:my-0 !shadow-2xl !rounded-3xl'>
+            <Box flexGrow={1}>
+            <IoChatboxEllipsesSharp />
+            
+            <Box padding={5}>
+            <p className='text-xl font-medium text-gray-800 pb-2'>Chat Support</p>
+            <p>Chat online with our team now from anywhere. We provide you with a seamless and hassle-free online chat experience from the comfort of your home. Get the proper assistance now!</p>
             </Box>
-
-            <Box>
-              <p className="text-xl font-bold">Cash On Delivery</p>
-
-              <p className="text-sm">Your order is in our safe hands</p>
             </Box>
-          </Flex>
-        </Card>
-
-        <Card p={22} className=" !shadow-lg">
-          <Flex gap={10} mx={15}>
-            <Box >
-              <Image
-                src="/support/support.png"
-                width={64}
-                height={64}
-                className="!w-[64px] !h-[64px]"
-              />
+            <Button fontSize={18} mt='4' bg={'red'} textColor={'white'} _hover={{bg:"red.500"}}>Chat Now</Button>
+            </Card>
+            
+            <Card flexDirection={'column'} borderRadius={10} gap={5} className='my-6 md:my-0 !shadow-2xl !rounded-3xl'>
+            <Box flexGrow={1}>
+            <FaPhone />
+            <Box padding={5}>
+            <p className='text-xl font-medium text-gray-800 pb-2'>Call Support</p>
+            <p>Reach out to us with your questions, concerns, or challenges. We’ll be happy to help you at any time, and we’re always trying to make things easier for you!</p>
             </Box>
-
-            <Box>
-              <p className="text-xl font-bold">Dedicated Support</p>
-
-              <p className="text-sm">Customer service all the time</p>
             </Box>
-          </Flex>
-        </Card>
+            <Button fontSize={18} mt='4' bg={'red'} textColor={'white'} _hover={{bg:"red.500"}}>Call Now</Button>
+            </Card>
+            
 
-        <Card p={22} className=" !shadow-lg">
-          <Flex gap={10} mx={15}>
-            <Box >
-              <Image
-                src="/support/money.png"
-                width={64}
-                height={64}
-                className="!w-[64px] !h-[64px]"
-              />
+
+            <Card flexDirection={'column'} borderRadius={10} gap={5} className='my-6 md:my-0 !shadow-2xl !rounded-3xl'>
+                <Box flexGrow={1}>
+            <FaCalendar />
+            <Box padding={5}>
+            <p className='text-xl font-medium text-gray-800 pb-2'>Book an Appointment</p>
+            <p>Now book an appointment with any of your nearest locations of GarbhaGudi IVF Centre, and get the best fertility treatments from our experts!</p>
             </Box>
-
-            <Box>
-              <p className="text-xl font-bold">Money Back Guarantee</p>
-
-              <p className="text-sm">
-                We guarantee the worthiness of every money transaction
-              </p>
             </Box>
-          </Flex>
-        </Card>
-      </SimpleGrid>
-    </div>
-  );
-};
+            
+            <Button fontSize={18} mt='4' bg={'red'} textColor={'white'} _hover={{bg:"red.500"}}>Book an Appointment</Button>
+            </Card>
+            
+            </SimpleGrid>
+        </div>
+    );
+}
 
 export default Support;
