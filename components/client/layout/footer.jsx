e4a13f32281554cccecf08efb9075b80f7519fa0
Zaher Zeinni <zaherzeinni@hotmail.com>
2024-01-31 00:45:48 +0200

    `update`

--- components/client/layout/footer.jsx
+++ components/client/layout/footer.jsx
@@ -2,17 +2,30 @@
 import React from 'react'
 import { Fade } from 'react-awesome-reveal'
 import Link from 'next/link'
-
 import {WhatsappShareButton,WhatsappIcon} from 'react-share'
 
 export default function Footer() {
+
+
+
 
     const shareUrl = "url";
     const title = "Title";
     
     return (
-
-            <div className='bg-sky-700  bottom-0 left-0 right-0 sm:mx-[-56px]1 mx-[-56px] '>
+            <div className=''>
+            
+            <svg xmlns="http://www.w3.org/2000/svg" 
+            width="32" 
+            height="32" 
+            fill="currentColor"
+            className="bi bi-arrow-up-circle mb-1  ml-auto hover:cursor-pointer hover:scroll-mt-80" 
+            viewBox="0 0 16 16"> 
+            
+            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z" fill="blue"></path> 
+            </svg>
+            
+            <div className='bg-sky-700  bottom-0 left-0 right-0 mx-[-60px] lg:mx-[-65px]  '>
                 <h1
                     className='
                     text-2xl font-bold tracking-wide text-center pt-6 text-gray-100 sm:text-3xl md:text-4xl h-2'
@@ -177,6 +190,6 @@ export default function Footer() {
                     </Fade>
                 </div>
             </div>
-        
+    </div>       
     )
 }
