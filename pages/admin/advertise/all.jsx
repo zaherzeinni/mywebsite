9b615747ab868447f218cbaf567321f04b504350
Zaher Zeinni <zaherzeinni@gmail.com>
2024-05-13 00:37:31 +0300

    okkk

--- pages/admin/advertise/all.jsx
+++ pages/admin/advertise/all.jsx
@@ -1,34 +1,34 @@
-import React from 'react';
-import AdvertisesMain from '@/components/admin/advertise/advertises';
-import { getDocuments } from '@/functions/firebase/getData';
+// import React from 'react';
+// import AdvertisesMain from '@/components/admin/advertise/advertises';
+// import { getDocuments } from '@/functions/firebase/getData';
 
-const All = ({adverts}) => {
-    return (
-        <div>
-            <AdvertisesMain
-            adverts={adverts}
-            />
-        </div>
-    );
-}
+// const All = ({adverts}) => {
+//     return (
+//         <div>
+//             <AdvertisesMain
+//             adverts={adverts}
+//             />
+//         </div>
+//     );
+// }
 
-export default All;
+// export default All;
 
 
 
-// serverside
-    All.getInitialProps = async (context) => {
-    const Advertises = await getDocuments("adverts"); //  []
+// // serverside
+//     All.getInitialProps = async (context) => {
+//     const Advertises = await getDocuments("adverts"); //  []
   
   
-    console.log("data", Advertises);
+//     console.log("data", Advertises);
   
   
-    return {
-      // props from serverside will go to props in clientside
-      adverts: Advertises,
-    };
-  };
+//     return {
+//       // props from serverside will go to props in clientside
+//       adverts: Advertises,
+//     };
+//   };
   
   
   
