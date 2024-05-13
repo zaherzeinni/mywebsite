b02427a8976f2aa2b681a8e4f33ef458e6b09a05
zaherzeinni <zaherzeinni@hotmail.com>
2024-05-13 13:16:48 +0300

    okk

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,7 +6,6 @@ export default class Bazaar extends Document {
     return <Html >
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700;900&display=swap" rel="stylesheet" />
-          {/* <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" /> */}
           
           
           
