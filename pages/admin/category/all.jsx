5421c7164924dc66fa19586b5b9ad9cb46b345c7
zaherzeinni <zaherzeinni@hotmail.com>
2024-05-13 12:29:55 +0300

    no admin/cat/all

--- pages/admin/category/all.jsx
+++ pages/admin/category/all.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import CategoriesMain from "@/components/admin/category/Categories";
-import { getDocuments } from "@/functions/firebase/getData";
+// import React from "react";
+// import CategoriesMain from "@/components/admin/category/Categories";
+// import { getDocuments } from "@/functions/firebase/getData";
 
-const All = ({ cats }) => {
-  return (
-    <div>
-      <CategoriesMain cats={cats} />
-    </div>
-  );
-};
-export default All;
+// const All = ({ cats }) => {
+//   return (
+//     <div>
+//       <CategoriesMain cats={cats} />
+//     </div>
+//   );
+// };
+// export default All;
 
-// serverside
-All.getInitialProps = async (context) => {
-  const Categories = await getDocuments("cats"); //  []
+// // serverside
+// All.getInitialProps = async (context) => {
+//   const Categories = await getDocuments("cats"); //  []
 
-  //console.log("data", Categories);
+//   //console.log("data", Categories);
 
-  return {
-    // props from serverside will go to props in clientside
-    cats: Categories,
-  };
-};
+//   return {
+//     // props from serverside will go to props in clientside
+//     cats: Categories,
+//   };
+// };
