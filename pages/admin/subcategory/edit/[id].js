65903845cc4719ce25e7475bbf60661f5eecbed5
zaherzeinni <zaherzeinni@hotmail.com>
2024-05-13 11:48:14 +0300

    kkk

--- pages/admin/subcategory/edit/[id].js
+++ pages/admin/subcategory/edit/[id].js
@@ -1,34 +1,26 @@
-import React from 'react';
-import  UpdateSubCategoryMain from '@/components/admin/subCategory/updateSubCategory';
-import { getDocuments,getDocument } from '@/functions/firebase/getData';
-const EditSubPage = ({cats,subcat}) => {
-    return (
-        <div>
-            <UpdateSubCategoryMain 
-            cats={cats}
-            subcat={subcat}
-
-            />
-        </div>
-    );
-}
-
+import React from "react";
+import UpdateSubCategoryMain from "@/components/admin/subCategory/updateSubCategory";
+import { getDocuments, getDocument } from "@/functions/firebase/getData";
+const EditSubPage = ({ cats, subcat }) => {
+  return (
+    <div>
+      <UpdateSubCategoryMain cats={cats} subcat={subcat} />
+    </div>
+  );
+};
 
 export default EditSubPage;
 
-
-
-
 // serverside
 EditSubPage.getInitialProps = async (context) => {
-    const Categories = await getDocuments("cats"); //  []
-    const subcat = await getDocument("subcats", context.query.id);
-    
-    console.log("data", Categories);
- 
-    return {
-      // props from serverside will go to props in clientside
-      cats: Categories,
-      subcat: subcat,
-    };
+  const Categories = await getDocuments("cats"); //  []
+  const subcat = await getDocument("subcats", context.query.id);
+
+  console.log("data", Categories);
+
+  return {
+    // props from serverside will go to props in clientside
+    cats: Categories,
+    subcat: subcat,
   };
+};
