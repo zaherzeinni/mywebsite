a1c704f074c5f03cf6b6acdd008ab97af8d47c7a
Zaher Zeinni <zaherzeinni@gmail.com>
2024-05-15 02:16:24 +0300

    url whatsapp

--- pages/products/productdetails/single.jsx
+++ pages/products/productdetails/single.jsx
@@ -1,4 +1,3 @@
-
 // product details of singleproduct
 
 import Link from "next/link";
@@ -8,10 +7,18 @@ import { useRouter } from "next/router";
 import parse from "html-react-parser";
 import Loader from "@/components/common/Loader";
 //import { Avatar, Box, Button, Chip, SimpleGrid, Container } from "@mui/material";
-import { Box, Button, SimpleGrid, Container, Flex, Image,Grid } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  SimpleGrid,
+  Container,
+  Flex,
+  Image,
+  Grid,
+} from "@chakra-ui/react";
 //import LazyImage from "components/LazyImage";
 //import BazaarRating from "components/BazaarRating";
-import { H1, H2, H3,H4,H5, H6 } from "components/Typography";
+import { H1, H2, H3, H4, H5, H6 } from "components/Typography";
 //import { useAppContext } from "function/context/AppContext";
 //import { FlexBox, FlexRowCenter } from "components/ProjectComponents/flex-box";
 //import { currency } from "lib";
@@ -32,7 +39,6 @@ import { chakra } from "@chakra-ui/react";
 import { FcCheckmark } from "react-icons/fc";
 import { VscClose } from "react-icons/vsc";
 
-//import { usePathname, useSearchParams } from 'next/navigation'
 
 // ================================================================
 
@@ -94,125 +100,125 @@ export default function ProductSingle() {
   // HANDLE SELECT IMAGE
   const handleImageClick = (ind) => () => setSelectedImage(ind);
 
-
-  const [getUrl,setGetUrl]=useState()
-  const [whatsappUrl,setWhatsappUrl] = useState()
+  const [getUrl, setGetUrl] = useState();
+  const [whatsappUrl, setWhatsappUrl] = useState();
 
   useEffect(() => {
     const currentUrl = window.location.href;
-    console.log(currentUrl,"urlllllllll");
+    console.log(currentUrl, "urlllllllll");
     setGetUrl(currentUrl);
-    const getUrlWhatsapp = "https://api.whatsapp.com/send?phone=96170480041&amp;text=Hello%2C+I+want+reserve+this+product%0D%0A%0D%0A**%0D%0A%2F%0D%0AThank+you%21";
-    setWhatsappUrl(getUrlWhatsapp)
-    console.log("urll whatsapp",getUrlWhatsapp)
-},[])
-
-
-
-
+  
+  }, []);
 
   return (
     <div>
       <ClientLayout />
 
-        <Box className="">
-          {loading ? (
-            <Loader />
-          ) : (
-            <Box>
-            <SimpleGrid   gap={5}>
-           
-        <Box className=" justify-center items-center m-auto space-x-1 md:space-x-10 ">                         
+      <Box className="">
+        {loading ? (
+          <Loader />
+        ) : (
+          <Box>
+            <SimpleGrid gap={5}>
+              <Box className=" justify-center items-center m-auto space-x-1 md:space-x-10 ">
+                <Box className="grid md:flex justify-center align-middle space-x-10 ">
+                  <Box className="lg:grid">
+                    {/* -------------------Product Image---------------------- */}
+                    <Box mb={2} className=" rounded-xl md:mt-3 mt-2 mx-5 ">
+                      {product?.images && product?.images[0] && (
+                        <Image
+                          alt={product?.title}
+                          src={
+                            product?.images && product?.images[selectedImage]
+                          }
+                          className="sm:w-[30vw] md:w-[30vw] justify-center flex m-auto rounded-lg object-contain"
+                        />
+                      )}
+                    </Box>
 
-               
+                    {/* --------------small images to select----------------- */}
 
-                
-        <Box className="grid md:flex justify-center align-middle space-x-10 ">
-
-              <Box className="lg:grid">
-                {/* -------------------Product Image---------------------- */}
-                <Box  mb={2} className=" rounded-xl md:mt-3 mt-2 mx-5 ">
-                  {product?.images && product?.images[0] && (
-                    <Image
-                      alt={product?.title}
-                      src={product?.images && product?.images[selectedImage]}
-                      className="sm:w-[30vw] md:w-[30vw] justify-center flex m-auto rounded-lg object-contain"
-                    />
-                  )}
-                </Box>
-                    
-
-         {/* --------------small images to select----------------- */}
-
-         <Box className="flex justify-center m-auto md:w-[500px] w-auto my-6 ">
-                  {product?.images &&
-                    product?.images.map((url, ind) => (
-                      <Box
-                      className="w-[40px] md:w-[60px]  m-auto   rounded-2xl  border-[1px] cursor-pointer"
-                        key={ind}
-                        ml={ind === 0 ? "auto" : 0}
-                        
-                        onClick={handleImageClick(ind)}
-                        mr={
-                          ind === product?.images.length - 1 ? "auto" : "10px"
-                        }
-                        borderColor={
-                          selectedImage === ind ? "primary.main" : "grey.400"
-                        }
-                      >
-                        <Image  className=' rounded-lg  ' src={url}  />
-                      </Box>
-                    ))}
-                </Box>
-                
-              </Box>
-
-
-                    {/* --------------Title and Details----------------- */}
-                  <Box className=" justify-center m-auto">
-               
-                <H1 mb={1} className='text-justify w-[70%] md:w-auto'>{product.title}</H1>
-
-                <Box alignItems="center" mb={1}>
-                  <Box>Brand:</Box>
-                  <H3>{product.subcategory}</H3>
-                </Box>
-
-                  <Box className=" lg:w-[56%] w-[40%] ">
-                    {product?.desc}
+                    <Box className="flex justify-center m-auto md:w-[500px] w-auto my-6 ">
+                      {product?.images &&
+                        product?.images.map((url, ind) => (
+                          <Box
+                            className="w-[40px] md:w-[60px]  m-auto   rounded-2xl  border-[1px] cursor-pointer"
+                            key={ind}
+                            ml={ind === 0 ? "auto" : 0}
+                            onClick={handleImageClick(ind)}
+                            mr={
+                              ind === product?.images.length - 1
+                                ? "auto"
+                                : "10px"
+                            }
+                            borderColor={
+                              selectedImage === ind
+                                ? "primary.main"
+                                : "grey.400"
+                            }
+                          >
+                            <Image className=" rounded-lg  " src={url} />
+                          </Box>
+                        ))}
+                    </Box>
                   </Box>
-        
 
+                  {/* --------------Title and Details----------------- */}
+                  <Box className=" justify-center m-auto">
+                    <H1 mb={1} className="text-justify w-[70%] md:w-auto">
+                      {product.title}
+                    </H1>
 
+                    <Box alignItems="center" mb={1}>
+                      <Box>Brand:</Box>
+                      <H3>{product.subcategory}</H3>
+                    </Box>
 
-           
+                    <Box className=" lg:w-[56%] w-[40%] ">{product?.desc}</Box>
 
-                <Box pt={1} my={5}>
-                  <H4 color="primary.main" mb={0.5} lineHeight="1">
-                    $ {product.price}
-                  </H4>
-                  <Box color="inherit">{product.instock===true ? <Flex gap={1}><FcCheckmark className="text-lg top-1 relative" /><H5>In stock</H5></Flex> : <Flex gap={1}><VscClose className="text-red-600 text-lg top-1 relative" /><H5 className="text-red-600">Out of stock</H5></Flex>}</Box>
-                </Box>
+                    <Box pt={1} my={5}>
+                      <H4 color="primary.main" mb={0.5} lineHeight="1">
+                        $ {product.price}
+                      </H4>
+                      <Box color="inherit">
+                        {product.instock === true ? (
+                          <Flex gap={1}>
+                            <FcCheckmark className="text-lg top-1 relative" />
+                            <H5>In stock</H5>
+                          </Flex>
+                        ) : (
+                          <Flex gap={1}>
+                            <VscClose className="text-red-600 text-lg top-1 relative" />
+                            <H5 className="text-red-600">Out of stock</H5>
+                          </Flex>
+                        )}
+                      </Box>
+                    </Box>
 
-                <Box alignItems="center" mb={2}>
-                  <Box></Box>
-                  {product.instock===true ? 
-                  <Link  href={whatsappUrl} target="_blank">
-                  
-                  <Button bg={"green.400"}  
-                  className=" hover:!bg-green-700 hover:animate-ping1 !text-white"  ml={1}>Reserve this Product</Button >
-                  
-                  </Link>
-                    : 
-                    <Button className=" hidden hover:!bg-white !bg-white cursor-default"></Button>}
-                </Box>
-{/* 
+                    <Box alignItems="center" mb={2}>
+                      <Box></Box>
+                      {product.instock === true ? (
+                        <Link href={`https://api.whatsapp.com/send?phone=96170480041&text=${getUrl} %0D%0A Hello I want to reserve this product  `} 
+                        
+                        target="_blank">
+                          
+                          
+                          <Button
+                            bg={"green.400"}
+                            className=" hover:!bg-green-700 hover:animate-ping1 !text-white"
+                            ml={1}
+                          >
+                            Reserve this Product
+                          </Button>
+                        </Link>
+                      ) : (
+                        <Button className=" hidden hover:!bg-white !bg-white cursor-default"></Button>
+                      )}
+                    </Box>
+                    {/* 
                 "https://api.whatsapp.com/send?phone=96170480041&amp;text=Hello%2C+I+want+reserve+this+product%0D%0A%0D%0A**%0D%0A%2F%0D%0AThank+you%21" */}
 
-
-                {/* href={`/products/productdetails/single?id=${data.id}`} */}
-
-
+                    {/* href={`/products/productdetails/single?id=${data.id}`} */}
                   </Box>
                   {/* in whatsApp */}
                   {/* one space */}
@@ -223,30 +229,18 @@ export default function ProductSingle() {
                   {/* %2C */}
                   {/* space */}
                   {/* + */}
-                  
-        </Box>
-       
-                    
-                {/* <ProductDescription  /> */}
-        </Box>
-            
-     
-      
-            </SimpleGrid>
-           
-            </Box>
-          )}
+                </Box>
 
-  
-        </Box>
-  
+                {/* <ProductDescription  /> */}
+              </Box>
+            </SimpleGrid>
+          </Box>
+        )}
+      </Box>
+
       <Footer />
     </div>
   );
 }
-
-
-
-
 
 // <a href="https://api.whatsapp.com/send?phone=96170480041&amp;text=Hello%2C+I+want+reserve+this+product%0D%0A%0D%0A*Open+Box+iPhone+15+Plus+Collection*%0D%0A*Price:* 995%C2%A0%24%0D%0A*URL:* https%3A%2F%2Fwww.khatwtelephone.com%2Flb%2Fproduct%2Fopen-box-iphone-15-plus-collection%2F%0D%0AThank+you%21" target="_blank"><Button bg={"green.400"}  className=" hover:!bg-green-700 hover:animate-ping1 !text-white"  ml={1}>Reserve this Product</Button ></a>
