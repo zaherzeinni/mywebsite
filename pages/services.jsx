6f108fec28e6ff5643d8c22b4c2fbed0ba046b84
zaherzeinni <zaherzeinni@hotmail.com>
2024-07-02 17:29:51 +0300

    services

--- pages/services.jsx
+++ pages/services.jsx
@@ -12,11 +12,43 @@ export default function Services() {
 
       <AdvertiseBar />
       <Navbar />
-
+      {/* -------------------------- new task development website-------------------  */}
+    
+      <div>
       <img src='https://www.spcdn.org/blog/wp-content/uploads/2023/07/coming-soon-page-cover.png'
       
       className='w-full h-full object-fill'
       />
+      </div>
+
+      <div className='hidden'> 
+      
+      
+      <div>
+        <img src='/web-development/Web-dev-spinningCircles.png' alt='image' />
+      </div>
+      <div>
+      <img src='/web-development/about-img-w1.jpg' alt='image' />
+      </div>
+      <div>
+      <img src='/web-development/about-img-w2.jpg' alt='image' />
+      </div>
+      <div>
+      <img src='/web-development/about-img-w3.jpg' alt='image' />
+      </div>
+      
+
+      <div className='m-3'>
+      <video width="320" height="240" controls>
+      <source src="/web-development/Web-dev-spinningCircles-video.mp4" type="video/mp4"/>
+  
+Your browser does not support the video tag.
+    </video>
+      </div>
+
+      </div>
+
+
       <Footer />
     </div>
   )
